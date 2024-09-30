8a50bf17c71e169d87db0aeb2eed5bc15be0b04d
Me <ga>
2024-09-29 23:14:09 -0500

    Introduced a Simple Sidebar Design
    Added sidebar with navigations currently not working but good ui visuals

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: "Gabriel", lastName: "Me" };
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
