c71155ac73923f180b3f7b16508ff9074b2ca400
Me <ga>
2024-09-30 01:04:51 -0500

    Upadted mobile sidebar navigator UI

--- components/ui/MobileNav.tsx
+++ components/ui/MobileNav.tsx
@@ -7,12 +7,21 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Image from "next/image";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   return (
-    <section>
+    <section className="w-full max-w-[264px]">
       <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
+        <SheetTrigger>
+          <Image
+            src="icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
         <SheetContent>
           <SheetHeader>
             <SheetTitle>Are you absolutely sure?</SheetTitle>
