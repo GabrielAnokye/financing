71d839e71c6b8d67bccce499a59a58c56bbcd79f
Me <gabrielyeboahanokye532@gmail.com>
2024-10-14 23:21:49 -0500

    Right Sidebar
    Created a basic right sidebar for further development

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,37 @@
-import HeaderBox from "@/components/HeaderBox";
+import HeaderBox from "@/components/ui/HeaderBox";
+import RightSidebar from "@/components/ui/RightSidebar";
 import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 import React from "react";
 
-
-
 const Home = () => {
-
   const loggedIn = {
     firstName: "Gabriel",
-  }
+    lastName: "Mine",
+    email: "contact@gmail.com",
+  };
 
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome"
             user={loggedIn?.firstName ?? "Guest"}
-            subtext="Access and manage your account and transactions efficiently." 
+            subtext="Access and manage your account and transactions efficiently. This app is highly reliable and secured."
           />
           <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1440.45}
-           />
+          />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
-}
+};
 
 export default Home;
